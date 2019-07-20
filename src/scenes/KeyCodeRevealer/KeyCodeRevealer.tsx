90454fb20cde002b91b7d39af0fd8aad959983cc
John Darryl <johndarrylpelingo@gmail.com>
2019-07-21 00:02:33 +0200

    Initial groundwork for key code reavealer

---
+++ src/scenes/KeyCodeRevealer/KeyCodeRevealer.tsx
@@ -0,0 +1,6 @@
+import { RouteComponentProps } from '@reach/router'
+import React, { FC } from 'react'
+
+export const KeyCodeRevealer: FC<RouteComponentProps> = () => {
+  return <div>KeyCodeRevealer</div>
+}
---
+++ src/scenes/KeyCodeRevealer/index.ts
@@ -0,0 +1,1 @@
+export { KeyCodeRevealer as default } from './KeyCodeRevealer'
