5d427b20f36430ef02993a3ba51fc72170eef5df
John Darryl <johndarrylpelingo@gmail.com>
2019-08-22 19:09:29 +0200

    Upgrade dependencies

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,9 +2,6 @@
 // doing this locally will remove type problems
 // import '@testing-library/jest-dom/extend-expect'
 
-// this is basically: afterEach(cleanup)
-import '@testing-library/react/cleanup-after-each'
-
 // Fail tests on any warning
 // console.error = message => {
 //   throw new Error(message)
