ba3643c67584bc5f6679c081d1d20433647a230e
John Darryl <johndarrylpelingo@gmail.com>
2019-10-05 01:46:27 +0200

    Add rest of cases

--- src/scenes/TextConverter/useTextConverter/reducer.ts
+++ src/scenes/TextConverter/useTextConverter/reducer.ts
@@ -1,6 +1,23 @@
 import produce from 'immer'
 
-import { SET_CAMEL_CASE } from './actions'
+import {
+  SET_CAMEL_CASE,
+  SET_CONSTANT_CASE,
+  SET_DOT_CASE,
+  SET_HEADER_CASE,
+  SET_LOWER_CASE,
+  SET_LOWER_FIRST_CASE,
+  SET_NO_CASE,
+  SET_PARAM_CASE,
+  SET_PATH_CASE,
+  SET_SARCASM_CASE,
+  SET_SENTENCE_CASE,
+  SET_SNAKE_CASE,
+  SET_SWAP_CASE,
+  SET_TITLE_CASE,
+  SET_UPPER_CASE,
+  SET_UPPER_FIRST_CASE,
+} from './actions'
 import { RootActions } from './types'
 
 import changeCase from 'change-case'
@@ -18,10 +35,70 @@ export const textConverterReducer = (
   action: RootActions,
 ): ITextConverterState => {
   return produce(state, draft => {
-    // TODO: add rest of cases
     switch (action.type) {
       case SET_CAMEL_CASE:
-        draft.text = changeCase.camelCase(action.payload.text)
+        draft.text = changeCase.camel(action.payload.text)
+        break
+
+      case SET_CONSTANT_CASE:
+        draft.text = changeCase.constant(action.payload.text)
+        break
+
+      case SET_DOT_CASE:
+        draft.text = changeCase.dot(action.payload.text)
+        break
+
+      case SET_HEADER_CASE:
+        draft.text = changeCase.header(action.payload.text)
+        break
+
+      case SET_LOWER_CASE:
+        draft.text = changeCase.lower(action.payload.text)
+        break
+
+      case SET_LOWER_FIRST_CASE:
+        draft.text = changeCase.lcFirst(action.payload.text)
+        break
+
+      case SET_NO_CASE:
+        draft.text = changeCase.no(action.payload.text)
+        break
+
+      case SET_PARAM_CASE:
+        draft.text = changeCase.param(action.payload.text)
+        break
+
+      case SET_PATH_CASE:
+        draft.text = changeCase.path(action.payload.text)
+        break
+
+      // TODO: implement
+      case SET_SARCASM_CASE:
+        draft.text = action.payload.text
+        break
+
+      case SET_SENTENCE_CASE:
+        draft.text = changeCase.sentence(action.payload.text)
+        break
+
+      case SET_SNAKE_CASE:
+        draft.text = changeCase.snake(action.payload.text)
+        break
+
+      case SET_SWAP_CASE:
+        draft.text = changeCase.swap(action.payload.text)
+        break
+
+      case SET_TITLE_CASE:
+        draft.text = changeCase.title(action.payload.text)
+        break
+
+      case SET_UPPER_CASE:
+        draft.text = changeCase.upper(action.payload.text)
+        break
+
+      case SET_UPPER_FIRST_CASE:
+        draft.text = changeCase.ucFirst(action.payload.text)
         break
     }
   })
